08d0541affbdc93bdc0ad94eec7174b3b315c912
TamatMiguel18 <mtamat-2024024@kinal.edu.gt>
2026-02-26 21:15:44 -0600

    Se agrego la interfas de Empleado

---
+++ src/AuthService.Domain/Constants/RoleContstants.cs
@@ -0,0 +1,10 @@
+namespace AuthService.Domain.Constants;
+
+public class Constants
+{
+    public const string ADMIN_ROLE = "ADMIN_ROLE";
+    public const string USER_ROLE = "USER_ROLE";
+
+    // Roles permitidos
+    public static readonly string[] AllowedRoles = { ADMIN_ROLE, USER_ROLE };
+}
