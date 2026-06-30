3f8086256f5b06ef8dd86499d530df3d5d585a8a
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-06-30 11:48:12 -0600

    refactor: modificar UserDetailDto para permitir el envio del email desde el servicio

--- src/AuthService.Application/Dtos/UserDetailsDto.cs
+++ src/AuthService.Application/Dtos/UserDetailsDto.cs
@@ -4,5 +4,6 @@ public class UserDetailsDto
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
 }
